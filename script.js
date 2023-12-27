13704373b762826f5eaceda0c7e120e5ab849da3
Shartechs <shartechs@gmail.com>
2023-12-27 20:16:09 +0400

    initial project setup

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 450;
+canvas.height = 500;
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 25, 25);
