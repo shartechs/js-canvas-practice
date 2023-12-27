7ca5f2e3cb24f9fb434d8e55da471e9c91a377d2
Shartechs <shartechs@gmail.com>
2023-12-28 00:15:37 +0400

    simple ball diagonal animation

--- script.js
+++ script.js
@@ -3,5 +3,30 @@ const ctx = canvas.getContext("2d");
 canvas.width = 450;
 canvas.height = 500;
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 25, 25);
+const ballPos = {
+  x: 0,
+  y: 0,
+  r: 30,
+  xSpeed: 1,
+  ySpeed: 1,
+};
+
+const drawBall = () => {
+  ballPos.x += ballPos.xSpeed;
+  ballPos.y += ballPos.ySpeed;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "green";
+  ctx.beginPath();
+
+  ctx.arc(ballPos.x, ballPos.y, ballPos.r, 0, 2 * Math.PI);
+  ctx.fill();
+};
+const update = () => {
+  drawBall();
+  requestAnimationFrame(update);
+};
+
+update();
+
+console.log(canvas);
