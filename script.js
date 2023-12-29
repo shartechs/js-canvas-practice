367ff6ed33dc3d179b8c5fb0b8f41315a1482caf
Shartechs <shartechs@gmail.com>
2023-12-29 20:51:02 +0400

    added random colors on bounce

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 450;
 canvas.height = 500;
+ctx.fillStyle = "green";
 
 const ballPos = {
   x: 30,
@@ -9,20 +10,28 @@ const ballPos = {
   r: 30,
   xSpeed: 5,
   ySpeed: 5,
+  colorArr: ["red", "blue", "yellow", "purple", "brown", "white"],
 };
 
 const drawBall = () => {
   if (ballPos.x + ballPos.r > canvas.width || ballPos.x - ballPos.r < 0) {
     ballPos.xSpeed *= -1;
+    const item =
+      ballPos.colorArr[Math.floor(Math.random() * ballPos.colorArr.length)];
+
+    ctx.fillStyle = item;
   }
   if (ballPos.y + ballPos.r > canvas.height || ballPos.y - ballPos.r < 0) {
     ballPos.ySpeed *= -1;
+    const item =
+      ballPos.colorArr[Math.floor(Math.random() * ballPos.colorArr.length)];
+
+    ctx.fillStyle = item;
   }
   ballPos.x += ballPos.xSpeed;
   ballPos.y += ballPos.ySpeed;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "green";
   ctx.beginPath();
 
   ctx.arc(ballPos.x, ballPos.y, ballPos.r, 0, 2 * Math.PI);
